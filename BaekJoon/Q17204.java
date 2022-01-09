75284039b2a6dcfb557f836c33cfe8699ce0a8d6
S-HoChan <hoo12044@gmail.com>
2022-01-09 13:15:48 +0900

    20220109

---
+++ BaekJoon/Q17204.java
@@ -0,0 +1,34 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q17204 {   // 죽음의 게임
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        boolean[] visited = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        int cnt = 0;
+        int next = 0;
+        visited[0] = true;
+        while (true) {
+            cnt++;
+            next = arr[next];
+            if (next == k) break;
+            if (visited[next]) {
+                cnt = -1;
+                break;
+            }
+            visited[next] = true;
+        }
+        System.out.println(cnt);
+    }
+}
